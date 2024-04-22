354974aa9d364d1cc364314ec218aae4b135a082
Ayomikun98 <jamesayantunji98@gmail.com>
2024-04-22 21:18:29 +0100

    fixed backgroundimg

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
         <div className="navbar">
             <h1>
             <span className="Welcome2"> Ayomikun98 </span> GitHub Portfolio</h1>
-            <img className="img-png" src="/src/components/backgroundimg.png" alt=""/>
+            <img className="img-png" src="/public/backgroundimg.png" alt=""/>
         </div>
         <button className="new-repo">My Repo</button>       
     </div>
